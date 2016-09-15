fc625964f50ef00d70bb5c0a9fa0ebee5199fec0
Roy Wallace <waldo000000@hotmail.com>
2016-09-15 21:09:28 +1000

    Add halt/resume button

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,11 +10,23 @@ view1.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 view1.controller('View1Ctrl', ['$scope', '$interval', function ($scope, $interval) {
+  var interval = 1000;
+      
   $scope.output = [];
+  
+  $scope.toggle = function toggle() {
+    if ($scope.running) {
+      $interval.cancel($scope.running);
+      $scope.running = null;
+    }
+    else {      
+      $scope.running = $interval(writeFrequencies, interval);
+    }
+  };
+  
+  $scope.toggle();
   
   function writeFrequencies() { 
     $scope.output = $scope.output.concat({ timestamp: new Date(), message: 'Frequencies to be written here' });
   }
-  
-  var writingFrequencies = $interval(writeFrequencies, 1000);
 }]);
