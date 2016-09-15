ebe9b93afdb239af88a79628d0074de353660e90
Roy Wallace <waldo000000@hotmail.com>
2016-09-15 20:43:16 +1000

    Write dummy messages each 1sec to view

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,20 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var view1 = angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+view1.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
-}])
+}]);
 
-.controller('View1Ctrl', [function() {
-
+view1.controller('View1Ctrl', ['$scope', '$interval', function ($scope, $interval) {
+  $scope.output = [];
+  
+  function writeFrequencies() { 
+    $scope.output = $scope.output.concat({ timestamp: new Date(), message: 'Frequencies to be written here' });
+  }
+  
+  var writingFrequencies = $interval(writeFrequencies, 1000);
 }]);
